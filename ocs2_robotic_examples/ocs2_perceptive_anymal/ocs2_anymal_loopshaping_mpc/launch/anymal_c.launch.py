ca1282a44d0f02ae6822220424d9240a628da29c
Huang Zhenbiao <biao876990970@hotmail.com>
2024-08-06 14:03:27 +0800

    fix the mpc launch file in anymal loopshaping mpc

--- ocs2_robotic_examples/ocs2_perceptive_anymal/ocs2_anymal_loopshaping_mpc/launch/anymal_c.launch.py
+++ ocs2_robotic_examples/ocs2_perceptive_anymal/ocs2_anymal_loopshaping_mpc/launch/anymal_c.launch.py
@@ -1,20 +1,18 @@
-import os
-import sys
-
-import launch
-import launch_ros.actions
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir
 from ament_index_python.packages import get_package_share_directory
 
 
-def generate_launch_description(): 
-    target_command = get_package_share_directory('ocs2_robotic_assets') + "/resources/anymal_c/urdf/anymal.urdf"
-    description_name = get_package_share_directory('ocs2_anymal_models') + "/urdf/anymal_camel_rsl.urdf"
-    ld = launch.LaunchDescription([
-        launch.actions.IncludeLaunchDescription(
-            launch.launch_description_sources.PythonLaunchDescriptionSource(
-                os.path.join(get_package_share_directory(
-                    'ocs2_anymal_loopshaping_mpc'), 'launch/mpc.launch.py')
-            ),
+def generate_launch_description():
+    target_command = get_package_share_directory('ocs2_anymal_mpc') + "/config/c_series/targetCommand.info"
+    description_name = get_package_share_directory('ocs2_robotic_assets') + "/resources/anymal_c/urdf/anymal.urdf"
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [ThisLaunchFileDir(), '/mpc.launch.py']),
             launch_arguments={
                 'robot_name': 'anymal_c',
                 'config_name': 'c_series',
@@ -23,8 +21,3 @@ def generate_launch_description():
             }.items()
         )
     ])
-    return ld
-
-
-if __name__ == '__main__':
-    generate_launch_description()
--- ocs2_robotic_examples/ocs2_perceptive_anymal/ocs2_anymal_loopshaping_mpc/launch/mpc.launch.py
+++ ocs2_robotic_examples/ocs2_perceptive_anymal/ocs2_anymal_loopshaping_mpc/launch/mpc.launch.py
@@ -1,87 +1,93 @@
-import os
-import sys
-
-import launch
-import launch_ros.actions
-from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition  
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+def is_wsl():
+    try:
+        with open('/proc/version', 'r') as f:
+            version_info = f.read().lower()
+            return 'microsoft' in version_info or 'wsl' in version_info
+    except FileNotFoundError:
+        return False
 
 def generate_launch_description():
-    ld = launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(
+    
+    prefix = "gnome-terminal --"
+    if is_wsl():
+        prefix = "xterm -e"
+        print("Current system is WSL, use xterm as terminal")
+    else:
+        print("Current system is not WSL, use gnome-terminal as terminal")
+        
+    return LaunchDescription([
+        DeclareLaunchArgument(
             name='robot_name'
         ),
-        launch.actions.DeclareLaunchArgument(
+        DeclareLaunchArgument(
             name='config_name'
         ),
         DeclareLaunchArgument(  
             name='rviz',  
             default_value='true',  
         ) ,
-        launch.actions.DeclareLaunchArgument(
+        DeclareLaunchArgument(
             name='description_name',
             default_value='ocs2_anymal_description'
         ),
-        launch.actions.DeclareLaunchArgument(
+        DeclareLaunchArgument(
             name='target_command',
             default_value=''
         ),
-        launch.actions.DeclareLaunchArgument(
+        DeclareLaunchArgument(
             name='urdf_model_path',
             default_value=get_package_share_directory('ocs2_robotic_assets') + "/resources/anymal_c/urdf/anymal.urdf"
         ),
-        launch_ros.actions.Node(
+        Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
             name='robot_state_publisher',
             output='screen',
             arguments=[LaunchConfiguration("urdf_model_path")],
         ),
-        launch.actions.IncludeLaunchDescription(
-            launch.launch_description_sources.PythonLaunchDescriptionSource(
-                os.path.join(get_package_share_directory(
-                    'ocs2_quadruped_interface'), 'launch/visualization.launch.py')
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                get_package_share_directory('ocs2_quadruped_interface') + "/launch/visualization.launch.py"
             ),
             launch_arguments={
                 'description_name': LaunchConfiguration('description_name'),
             }.items()
         ),
-        launch_ros.actions.Node(
+        Node(
             package='ocs2_anymal_loopshaping_mpc',
             executable='ocs2_anymal_loopshaping_mpc_mpc_node',
             name='ocs2_anymal_loopshaping_mpc_mpc_node',
-            prefix="",
             arguments=[LaunchConfiguration('description_name'), LaunchConfiguration('config_name')],
             output='screen'
         ),
-        launch_ros.actions.Node(
+        Node(
             package='ocs2_anymal_loopshaping_mpc',
             executable='ocs2_anymal_loopshaping_mpc_dummy_mrt_node',
             name='ocs2_anymal_loopshaping_mpc_dummy_mrt_node',
-            prefix="gnome-terminal --",
+            prefix=prefix,
             arguments=[LaunchConfiguration('description_name'), LaunchConfiguration('config_name')],
             output='screen'
         ),
-        launch_ros.actions.Node(
+        Node(
             package='ocs2_anymal_commands',
             executable='gait_command_node',
             name='gait_command_node',
-            prefix="gnome-terminal --",
+            prefix=prefix,
             output='screen'
         ),
-        launch_ros.actions.Node(
+        Node(
             package='ocs2_anymal_commands',
             executable='target_command_node',
             name='target_command_node',
-            prefix="gnome-terminal --",
+            prefix=prefix,
             arguments=[LaunchConfiguration('target_command')],
             output='screen'
-        )
+        ),
     ])
-    return ld
-
-
-if __name__ == '__main__':
-    generate_launch_description()
